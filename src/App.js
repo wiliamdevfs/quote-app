309b0dc44e74c2afc02188ed2527e55a5d55260f
rexxx <wiliamkieu.dev@gmail.com>
2022-07-02 00:19:18 +0700

    simple quote random

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      quote: "Random quote for you <3",
+    };
+  }
+
+  componentDidMount() {
+    this.fetchQuote();
+  }
+
+  fetchQuote = () => {
+    const url = "https://api.adviceslip.com/advice";
+
+    axios
+      .get(url)
+      .then((res) => {
+        const slipData = res.data.slip;
+        const adviceData = slipData.advice;
+
+        this.setState({
+          quote: adviceData,
+        });
+      })
+      .catch((err) => {
+        console.log("🚀 ~ file: App.js ~ line 24 ~ App ~ err", err);
+      });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{this.state.quote}</h1>
+
+          <button className="button" onClick={this.fetchQuote}>
+            <span>GIVE ME ADVICE!</span>
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
